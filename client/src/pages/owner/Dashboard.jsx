44132ef65a7ce78c50219adee3a6592f2c5070b2
Junaid Khan <pathanjunaid7888@gmail.com>
2026-01-06 13:22:17 +0530

    update

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyDashboardData } from '../../assets/assets';
 
 const Dashboard = () => {
+
+  const [data, setData] = useState({
+    totalCars: 0, 
+    totalBookings: 0, 
+    pendingBookings: 0, 
+    completBookings: 0, 
+    recentBookings: [],
+    monthlyRvenue: 0,
+  });
+
+  useEffect(()=>{
+    setData(dummyDashboardData)
+  },[])
   return (
-    <div>Dashboard</div>
+    <div className=''>
+
+    </div>
   )
 }
 
